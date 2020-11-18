6c3bbfb5aab55aafc3413459bd3c912c12a4992b
Julien Xemard <julien.xemard@outlook.com>
2020-11-18 14:58:35 +0100

    portFolio set-up

---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <h1>This the Projects Page</h1>
+  )
+}
+
+export default Projects
---
+++ src/containers/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import Landing from '../components/Landing'
+import About from '../components/About'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+import '../style/style.scss'
+
+const App = () => {
+
+  const [display, setDisplay] = useState(false)
+
+  function showContent() {
+    setDisplay(true)
+  }
+
+  return (
+    <div>
+      <Landing showContent={() => showContent()} />
+
+      {display ? <>
+        <About />
+        <Projects />
+        <Contact />
+      </>
+        : null}
+    </div>
+
+  )
+}
+
+export default App
