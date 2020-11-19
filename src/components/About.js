0579846244b817d05201744e9aa6cbc61863fe40
Julien <julien.xemard@outlook.com>
2020-11-19 19:15:31 +0100

    Tech init & set-up

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react'
+import { SvgIcon } from './Icons'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+
+const proficientSkills = ['Node.js', 'JavaScript', 'React.js', 'MongoDB', 'PostgreSQL', 'GitHub', 'VScode', 'HTML', 'CSS']
+
+const currentlyLearning = ['Python', 'Django', 'TypeScript', 'Jest']
+
 
 const About = () => {
 
@@ -36,7 +42,7 @@ const About = () => {
         data-aos-easing="ease">
         <p>
           Over the past 10 years, I evolved from Mid to Top Mangament positions until the creation
-          of my own firm within the <span>Space</span> Retail industry.<br/> Fascinated by the digital impact in this very industry amongst other subjects such as <span>Sports</span> Neuralink, Machine Learning, AR & Space,
+          of my own firm within the <span>Space</span> Retail industry.<br/> Fascinated by the digital impact in this very industry amongst other subjects such as Neuralink, Machine Learning, AR & Space,
           I undertook a professional reconversion.
         </p>
       </div>
@@ -58,8 +64,44 @@ const About = () => {
         data-aos-duration="800"
         data-aos-easing="ease">
         <p>
-          Off my screen<small>,</small> you will either find me <span>watching motor sports</span> on a Bresilien Jiu Jitsu mat or on a trail in the nature with Le Dog. I do not hate <span>pineapples on pizza</span> whiskey & wine on good occasions.
+          Off my screen<small>,</small> you will either find me on a Bresilien Jiu Jitsu mat or on a trail in the nature with Le Dog. I do not hate <span>pineapples on pizza</span> whiskey & wine on good occasions.
         </p>
+      </div>
+      <div id="para"
+        data-aos="fade-up"
+        data-aos-offset="250"
+        data-aos-duration="800"
+        data-aos-easing="ease">
+        <p>Currently Learning<small>:</small></p>
+        <div className="technologies">
+          {currentlyLearning.map(icon => (
+            <div key={icon} className="tech-logo">
+              <SvgIcon
+                className="svg"
+                width={60}
+                height={60}
+                icon={icon}
+                primaryFill={'#C67804'}
+                secondaryFill={'#48575e'}
+              />
+            </div>
+          ))}
+        </div>
+        <p>Proficient<small>:</small></p>
+        <div className="technologies">
+          {proficientSkills.map(icon => (
+            <div key={icon} className="tech-logo">
+              <SvgIcon
+                className="svg"
+                width={60}
+                height={60}
+                icon={icon}
+                primaryFill={'#C67804'}
+                secondaryFill={'#48575e'}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
